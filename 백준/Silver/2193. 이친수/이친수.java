08d20bab53c0195ec8e3f9e9b05baa3615010810
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-05-10 21:36:02 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		long[] dp = new long[n+1];
+		
+		dp[1]=1;
+		if(n>=2) {
+			dp[2]=1;
+		}
+		if(n>=3)
+		dp[3]=2;
+		for(int i=4; i<=n; i++) {
+			dp[i] = dp[i-1]+dp[i-2];
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
