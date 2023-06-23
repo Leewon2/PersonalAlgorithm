4eacb61d191f477244185fab6214740b7476a7ae
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-06-23 18:56:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 572 ms, Memory: 64292 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,10 +1,12 @@
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	
@@ -15,10 +17,10 @@ public class Main {
 	static ArrayList<Integer>[] list;
 	static Queue<Integer> q;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		N = sc.nextInt();
+		N = Integer.parseInt(br.readLine());
 		q= new LinkedList<>();
 		result = new int[N+1];
 		visited = new boolean[N+1];
@@ -27,17 +29,21 @@ public class Main {
 			list[i]=new ArrayList<>();
 		}
 		for(int i=0; i<N-1; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
 			list[x].add(y);
 			list[y].add(x);
 		}
 		q.offer(1);
 		visited[1]=true;
 		bfs();
+		StringBuilder sb = new StringBuilder();
 		for(int i=2; i<N+1; i++) {
-			System.out.println(result[i]);
+			sb.append(result[i]).append("\n");
 		}
+		System.out.println(sb);
 		
 	}
 	public static void bfs() {
