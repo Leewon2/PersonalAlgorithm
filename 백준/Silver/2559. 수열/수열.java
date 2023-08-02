4fed52b9ccbb19971170f47be48e0900c13e5254
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-08-03 01:20:14 +0900

    [Silver III] Title: 수열, Time: 200 ms, Memory: 22272 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int start=0;
+        for (int i = 0; i < K; i++) {
+            start += arr[i];
+        }
+        int max = start;
+        int compare = start;
+        for (int i = 1; i < N - K +1; i++) {
+            compare = compare - arr[i - 1] + arr[i + K -1];
+            max = Math.max(compare, max);
+        }
+        System.out.println(max);
+    }
+}
