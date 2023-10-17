f423546438e47bb0feabda59ce47118623513767
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-10-17 21:35:42 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 12992 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int[] compare = new int[M];
+        sb = new StringBuilder();
+        boolean[] check = new boolean[N];
+        backtracking(arr, N,M,0,compare, 0);
+        System.out.println(sb);
+    }
+
+    private static void backtracking(int[] arr, int N, int M, int idx, int[] compare,int start){
+        if(idx==M){
+            for (int i = 0; i < compare.length; i++) {
+                sb.append(compare[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+        for (int i = start; i < N; i++) {
+                compare[idx] = arr[i];
+                backtracking(arr, N, M, idx + 1, compare, i+1);
+        }
+    }
+}
