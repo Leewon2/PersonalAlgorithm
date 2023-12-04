98f64d878dbb3d1559546ea0f13640fc1fe50bc4
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-12-05 01:22:46 +0900

    [Silver I] Title: 국기 색칠하기, Time: 76 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Silver/30702. 국기 색칠하기/국기 색칠하기.java
+++ 백준/Silver/30702. 국기 색칠하기/국기 색칠하기.java
@@ -10,7 +10,7 @@ public class Main {
     static final int[] dr={1,0,-1,0};
     static final int[] dc={0,1,0,-1};
     static boolean[][] visited;
-    static boolean[][] visited2;
+//    static boolean[][] visited2;
     static int N;
     static int M;
     static char[][] A;
@@ -24,16 +24,16 @@ public class Main {
         A = new char[N][M];
         B = new char[N][M];
         visited = new boolean[N][M];
-        visited2 = new boolean[N][M];
+//        visited2 = new boolean[N][M];
         input(A,br);
         input(B,br);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if(A[i][j] != B[i][j] && !visited[i][j]){
                     visited[i][j]= true;
-                    visited2[i][j]= true;
-                    dfs(i,j);
-                    change(B[i][j]);
+//                    visited2[i][j]= true;
+                    dfs(i,j,A[i][j],B[i][j]);
+//                    change(B[i][j]);
                 }
             }
         }
@@ -50,29 +50,26 @@ public class Main {
         return "YES";
     }
 
-    private static void change(char X){
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if(visited2[i][j]){
-                    A[i][j] = X;
-                }
-            }
-        }
-        visited2 = new boolean[N][M];
-
-
-
-    }
-    private static void dfs(int r, int c){
-
+//    private static void change(char X){
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                if(visited2[i][j]){
+//                    A[i][j] = X;
+//                }
+//            }
+//        }
+//        visited2 = new boolean[N][M];
+//    }
+    private static void dfs(int r, int c, char a, char b){
+        A[r][c]=b;
         for (int i = 0; i < 4; i++) {
             int nr = r+dr[i];
             int nc = c+dc[i];
             if(nr<0 || nc<0 || nr>=N || nc>=M) continue;
-            if(!visited[nr][nc] && A[r][c]==A[nr][nc]){
+            if(!visited[nr][nc] && a==A[nr][nc]){
                 visited[nr][nc]=true;
-                visited2[nr][nc]=true;
-                dfs(nr,nc);
+//                visited2[nr][nc]=true;
+                dfs(nr,nc,a,b);
             }
         }
     }
