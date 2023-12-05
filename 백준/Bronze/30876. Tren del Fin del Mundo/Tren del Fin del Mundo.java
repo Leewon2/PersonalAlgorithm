4ff4457c12b53860d7af551f8d8e159c888fe426
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2023-12-05 20:47:46 +0900

    [Bronze IV] Title: Tren del Fin del Mundo, Time: 88 ms, Memory: 11976 KB -BaekjoonHub

---
+++ 백준/Bronze/30876. Tren del Fin del Mundo/Tren del Fin del Mundo.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int xx = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if(min>y){
+                xx = x;
+                min = y;
+            }
+        }
+        System.out.println(xx+" " +min);
+
+
+    }
+}
