20e8a66a47ec93f3a94af42ceb71729235d9d55c
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-03-12 15:42:24 +0900

    [Gold IV] Title: 줄세우기, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        int max=0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(dp[i],max);
+        }
+        System.out.println(N-max);
+    }
+}
