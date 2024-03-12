16ff55abdcee2018772ce67ee3355f1923fa3512
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-03-12 16:21:03 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1728 ms, Memory: 371604 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M,K;
+    static int[][] arr;
+    static Queue<Node> q;
+    static int[] dr={1,0,-1,0};
+    static int[] dc={0,1,0,-1};
+    static boolean[][][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M][K+1];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Character.getNumericValue(str.charAt(j));
+            }
+        }
+
+
+        q = new LinkedList<>();
+        q.offer(new Node(0, 0, 1,0));
+        visited[0][0][0] = true;
+        int result = bfs();
+        System.out.println(result==Integer.MAX_VALUE ? -1 : result);
+
+
+    }
+    private static int bfs(){
+        int res=Integer.MAX_VALUE;
+        while (!q.isEmpty()) {
+            Node poll = q.poll();
+            int r = poll.r;
+            int c = poll.c;
+            int cnt = poll.cnt;
+            int wall = poll.wall;
+            if(r==N-1 && c==M-1){
+                res = Math.min(res,cnt);
+                continue;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
+                // 그냥 갈 수 있는 경우
+                if(arr[nr][nc]==0 && !visited[nr][nc][wall]){
+                    q.offer(new Node(nr, nc, cnt + 1, wall));
+                    visited[nr][nc][wall] = true;
+                }
+                // 벽인 경우
+                else{
+                    // 갯수 다 안채웠으면 벽 깨고 이동
+                    if(wall<K && !visited[nr][nc][wall+1]){
+                        q.offer(new Node(nr, nc, cnt + 1, wall + 1));
+                        visited[nr][nc][wall+1] = true;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+    private static class Node {
+        int r;
+        int c;
+        int cnt;
+        int wall;
+
+        public Node(int r, int c, int cnt, int wall) {
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+            this.wall = wall;
+        }
+    }
+}
