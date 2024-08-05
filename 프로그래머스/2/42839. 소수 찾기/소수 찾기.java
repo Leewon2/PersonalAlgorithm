497a06271b1cf128fe58936e96c14290b5be71ae
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-08-06 00:23:54 +0900

    [level 2] Title: 소수 찾기, Time: 100.12 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -5,10 +5,17 @@ class Solution {
     static boolean[] visited;
     static Set<Integer> set;
     public int solution(String numbers) {
-        int answer = 0;
         visited = new boolean[numbers.length()];
-        isPrime = new boolean[10000000];
         set = new HashSet<>();
+        makeIsPrime();
+        for(int i=1; i<=numbers.length(); i++){
+            perm(i,numbers,"");
+        }
+        return set.size();
+    }
+    
+    private static void makeIsPrime(){
+        isPrime = new boolean[10000000];
         isPrime[0] = true;
         isPrime[1] = true;
         for(int i=2; i<Math.sqrt(10000000); i++){
@@ -18,27 +25,18 @@ class Solution {
                 }
             }
         }
-        for(int i=1; i<=numbers.length(); i++){
-            perm(i,numbers,"");
-        }
-        
-        for(int s : set){
-            if(!isPrime[s]) answer++;
-        }
-        
-        
-        return answer;
     }
+    
     private static void perm(int idx, String numbers, String compare){
         if(compare.length()==idx){
-            set.add(Integer.parseInt(compare));
+            if(!isPrime[Integer.parseInt(compare)])
+                set.add(Integer.parseInt(compare));
             return;
         }
         for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                String str = compare+numbers.charAt(i);
-                perm(idx, numbers, str);
+                perm(idx, numbers, compare+numbers.charAt(i));
                 visited[i] = false;
             }
         }
