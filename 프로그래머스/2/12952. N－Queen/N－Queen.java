80040842c19e384d9f70457f2456867c616d26d9
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-08-06 17:11:33 +0900

    [level 2] Title: N-Queen, Time: 362.49 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -8,8 +8,22 @@ class Solution {
         map = new boolean[n][n];
         answer = 0;
         DFS(0,n);
-        
         return answer;
+    }
+    private static boolean check(int r, int c, int n){
+        for(int d=0; d<3; d++){
+            int nr = r;
+            int nc = c;
+            while(nr>=0 && nc>=0 && nc<n){
+                nr += dr[d];
+                nc += dc[d];
+                if(nr<0 || nc<0 || nc>=n) break;
+                if(map[nr][nc]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     
     private static void DFS(int idx, int n){
@@ -18,21 +32,7 @@ class Solution {
             return;
         }
         for(int i=0; i<n; i++){
-            boolean check = true;
-            outer : for(int d=0; d<3; d++){
-                int nr = idx;
-                int nc = i;
-                while(nr>=0 && nc>=0 && nc<n){
-                    nr += dr[d];
-                    nc += dc[d];
-                    if(nr<0 || nc<0 || nr>=n || nc>=n) break;
-                    if(map[nr][nc]) {
-                        check = false;
-                        break outer;
-                    }
-                }
-            }
-            if(check){
+            if(check(idx,i, n)){
                 map[idx][i] = true;
                 DFS(idx+1,n);
                 map[idx][i]=false;
