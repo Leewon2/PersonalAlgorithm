1b1ad97dca59a4ee6b98789442304e40bd450c43
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-08-21 14:56:21 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -7,18 +7,28 @@ class Solution {
         for(int i=0; i<use.length; i++){
             if(!use[i]){
                 answer++;
-                q.offer(i);
-                while(!q.isEmpty()){
-                    int poll = q.poll();
-                    use[poll]=true;
-                    for(int j=0;j<use.length; j++){
-                        if(poll==j || computers[poll][j]==0 || use[j]) continue;
-                        q.offer(j);
-                    }
-                }
+                dfs(i,computers,use);
+                // q.offer(i);
+                // while(!q.isEmpty()){
+                //     int poll = q.poll();
+                //     use[poll]=true;
+                //     for(int j=0;j<use.length; j++){
+                //         if(poll==j || computers[poll][j]==0 || use[j]) continue;
+                //         q.offer(j);
+                //     }
+                // }
             }
         }
         
+        
         return answer;
+    }
+    private static void dfs(int idx, int[][] computers, boolean[]use){
+        
+        for(int j=0;j<use.length; j++){
+              if(idx==j || computers[idx][j]==0 || use[j]) continue;
+            use[j]=true;
+            dfs(j,computers,use);
+        }
     }
 }
