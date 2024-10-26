aa491e3dda001d0d6fa1ffef3ed019e5e27f2dfa
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2024-10-27 01:44:24 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 3.47 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -5,8 +5,6 @@ class Solution {
         int answer = 0;
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
-        int left = 0;
-        int right = 0;
         str1=str1.toUpperCase();
         str2=str2.toUpperCase();
         for(int i=0; i<str1.length()-1; i++){
@@ -14,26 +12,24 @@ class Solution {
             char r = str1.charAt(i+1);
             if(l-'A'<0 || l-'Z'>0 || r-'A'<0 || r-'Z'>0) continue;
             list1.add(Character.toString(l)+ Character.toString(r));
-            
         }
+        
         for(int i=0; i<str2.length()-1; i++){
             char l = str2.charAt(i);
             char r = str2.charAt(i+1);
             if(l-'A'<0 || l-'Z'>0 || r-'A'<0 || r-'Z'>0) continue;
             list2.add(Character.toString(l)+ Character.toString(r));
         }
-        boolean[] arr = new boolean[list2.size()];
         
+        int left = 0;
+        int right = list1.size()+list2.size();
         for(int i=0; i<list1.size(); i++){
-            for(int j=0; j<list2.size(); j++){
-                if(list1.get(i).equals(list2.get(j)) && !arr[j]){
-                    arr[j]=true;
-                    left++;
-                    break;
-                }
-            }    
+            if(list2.contains(list1.get(i))){
+                left++;
+                list2.remove(list1.get(i));
+            }
         }
-        right = list1.size()+list2.size()-left;
+        right -= left;
         if(right==0) return 65536;
         answer = left*65536/right;
         return answer;
