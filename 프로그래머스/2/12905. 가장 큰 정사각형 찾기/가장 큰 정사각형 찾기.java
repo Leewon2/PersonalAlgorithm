0f44a86896f3c4b8014b28c64321810fbf6df1cb
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2025-02-05 01:22:38 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 26.55 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -5,64 +5,41 @@ class Solution
     {
         int r = board.length;
         int c = board[0].length;
-        int answer = 1;
+        int answer = 0;
         int[][] suffix = new int[r][c];
         for(int i=0; i<r; i++){
             suffix[i][0]=board[i][0];
             for(int j=1;j<c;j++){
-                if(board[i][j]==0) {
-                    suffix[i][j]=0;
-                    continue;
-                }
+                if(board[i][j]==0) continue;
                 suffix[i][j] = suffix[i][j-1]+board[i][j];
             }
         }
         // 1인거 찾기
         for(int i=0; i<r; i++){
+            if(r-i<=answer) break;
             for(int j=c-1; j>=0; j--){
+                if(j+1<=answer) break;
                 if(answer>=suffix[i][j]) continue;
                 boolean check = true;
-                for(int k=0; k<suffix[i][j]; k++){
-                    if(i+k>=r || k>=c || suffix[i+k][j]<suffix[i][j]) {
+                int max = suffix[i][j];
+                for(int k=0; k<max; k++){
+                    if(i+k>=r) {
                         check = false;
                         break;
                     }
-                }
-                if(check) answer = suffix[i][j];
-            }
-        }
-        // 0인거 찾기
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                board[i][j] = Math.abs(board[i][j]-1);
-            }
-        }
-        suffix = new int[r][c];
-        for(int i=0; i<r; i++){
-            suffix[i][0]=board[i][0];
-            for(int j=1;j<c;j++){
-                if(board[i][j]==0) {
-                    suffix[i][j]=0;
-                    continue;
-                }
-                suffix[i][j] = suffix[i][j-1]+board[i][j];
-            }
-        }
-        for(int i=0; i<r; i++){
-            for(int j=c-1; j>=1; j--){
-                if(answer>=suffix[i][j]) continue;
-                boolean check = true;
-                for(int k=0; k<suffix[i][j]; k++){
-                    if(i+k>=r || k>=c || suffix[i+k][j]<suffix[i][j]) {
-                        check = false;
+                    if(suffix[i+k][j]>=max) {
+                        answer = Math.max(answer,k+1);
+                    }
+                    else {
+                        max = suffix[i+k][j];
+                        answer = Math.max(Math.min(k+1,max),answer);
+                    }
+                    if(max<=answer){
                         break;
                     }
                 }
-                if(check) answer = Math.max(answer,suffix[i][j]);
             }
         }
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-
         return answer*answer;
     }
 }
