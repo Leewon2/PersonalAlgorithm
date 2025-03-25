c5312bb10fbe4fedf832c3341b271cc2f3d42512
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2025-03-25 10:19:17 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.43 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+        for(int i=0; i<score.length; i++){
+            if(pq.isEmpty()|| pq.size()<k) {
+                pq.offer(score[i]);
+            }else{
+                if(pq.peek()<score[i]){
+                    pq.poll();
+                    pq.offer(score[i]);
+                }
+            }
+            answer[i]=pq.peek();
+            
+        }
+        return answer;
+    }
+}
