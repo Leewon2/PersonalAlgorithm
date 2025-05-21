e16f9d4eba46e9b1ce7d936e8a64b21055cfce0a
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2025-05-22 02:40:28 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a%2==1 && b%2==1) return a*a+b*b;
+        else if(a%2==1 || b%2==1) return 2*(a+b);
+        return Math.abs(a-b);
+    }
+}
