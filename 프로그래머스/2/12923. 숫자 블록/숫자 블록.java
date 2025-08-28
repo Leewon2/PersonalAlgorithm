652dad60477cea21902edc87484d80f9004245f9
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2025-08-28 12:54:37 +0900

    [level 2] Title: 숫자 블록, Time: 486.87 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(long begin, long end) {
+        int s = (int) begin;
+        int e = (int) end;
+        int[] answer = new int[e-s+1];
+        // 약수 중 자기 자신을 제외하고 가장 큰 수
+        int idx=0;
+        for(int i=s; i<=e; i++){
+            if(i==1) {
+                answer[idx++]=0;
+                continue;
+            }
+            int max=1;
+            for(int j=2; j*j<=i; j++){
+                if(j>10000000) break;
+                if(i%j==0){
+                    max=Math.max(max,j);
+                    if(j != i/j && i/j<=10000000){
+                        max=Math.max(max,i/j);
+                    }
+                }
+            }
+            answer[idx++] = max;
+        }
+        return answer;
+    }
+}
