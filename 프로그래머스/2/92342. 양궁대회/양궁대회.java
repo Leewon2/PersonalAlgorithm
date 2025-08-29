1977fda90609dc1f6f95216dea47193fa6d4068d
Leewon2 <107541923+Leewon2@users.noreply.github.com>
2025-08-30 02:10:44 +0900

    [level 2] Title: 양궁대회, Time: 0.69 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+    int max;
+    int[] answer;
+    public int[] solution(int n, int[] info) {
+        max=0;
+        int[] compare = new int[11];
+        dfs(0,n,info,compare);
+        if(max==0) return new int[]{-1};
+        return answer;
+    }
+    
+    private void dfs(int idx, int remain, int[] info, int[] compare){
+        if(remain<0) return;
+        if(idx==11 || remain<=0){
+            if(idx==11) {
+                compare[10]+=remain;
+            }else{
+                
+            }
+            int lionScore = calculate(compare,info);
+            if(lionScore>0){
+                if(lionScore>max){
+                    max = lionScore;
+                    answer = compare.clone();
+                } else if(lionScore==max){
+                    if (answer == null) {
+                        answer = compare.clone();
+                    } else {
+                        for (int i = 10; i >= 0; i--) {
+                            if (compare[i] > answer[i]) {
+                                answer = compare.clone();
+                                break;
+                            }else if(compare[i] < answer[i]) break;
+                        }
+                    }
+                }
+            }
+            
+            compare[10]-=remain;
+            return;
+        }
+        if(info[idx]>=remain){
+            dfs(idx+1, remain, info, compare);
+        }else{
+            compare[idx] = info[idx]+1;
+            dfs(idx+1, remain-compare[idx], info, compare);
+            compare[idx] = 0;
+            dfs(idx+1, remain, info, compare);
+        }
+    }
+    private int calculate(int[] arr1, int[] arr2){
+        int cnt=0;
+        for(int i=0; i<11; i++){
+            if(arr1[i]==0 && arr2[i]==0) continue;
+            if(arr1[i]>arr2[i])
+                cnt+=10-i;
+            else cnt-=(10-i);
+        }
+        return cnt;
+    }
+    
+}
